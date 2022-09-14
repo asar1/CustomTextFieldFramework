21eb3ba3e30c901aaca3fefaf676a1e42b668648
Mudassar388 <asarsaleem@gmail.com>
2022-09-14 19:26:04 +0500

    Project Added

---
+++ CustomTextFieldFramework/CustomTextFieldFramework.h
@@ -0,0 +1,18 @@
+//
+//  CustomTextFieldFramework.h
+//  CustomTextFieldFramework
+//
+//  Created by Muhammad Mudassar Yasin on 14/09/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomTextFieldFramework.
+FOUNDATION_EXPORT double CustomTextFieldFrameworkVersionNumber;
+
+//! Project version string for CustomTextFieldFramework.
+FOUNDATION_EXPORT const unsigned char CustomTextFieldFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomTextFieldFramework/PublicHeader.h>
+
+
